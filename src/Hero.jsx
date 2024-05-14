77f53f10e68ea1741f0b242e74f1455cffc37550
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-14 16:46:56 +0530

    added some content

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,15 +3,15 @@ import heroSVG from './assets/hero-2.svg'
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>My React-18 Projects </h1>
+      <h1>Personal Frontend Projects </h1>
       <div className="hero-center">
         <div className="hero-title">
           <p>
-            Discover my latest React-18 projects, powered by <em>Vite</em>. Leveraging{' '}
-            <em>Axios</em> for data fetching, <em>Context API</em> and <em>React Query</em> for
-            state management, and <em>TailwindCSS</em> for UI. With <em>React-Router-dom 6.4+</em>{' '}
-            for navigation and <em>Redux-Toolkit</em> for state, showcasing diverse skills in React
-            development.
+            Discover my latest Frontend JavaScript, HTML5 CSS3 projects & React-18 projects powered
+            by <em>Vite</em>. Leveraging <em>Axios</em> for data fetching, <em>Context API</em> and{' '}
+            <em>React Query</em> for state management, and <em>TailwindCSS</em> for UI. With{' '}
+            <em>React-Router-dom 6.4+</em> for navigation and <em>Redux-Toolkit</em> for state,
+            showcasing diverse skills in React development.
           </p>
         </div>
 
